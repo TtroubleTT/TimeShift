23736b817bddfea450fc9dc8e6005d7cf4d563b1
TtroubleTT <TtroubleTT2@gmail.com>
2024-05-14 00:12:51 -0700

    fixed bug with door moving

--- Time Shift/Assets/Scripts/Enviornment/OpenDoor.cs
+++ Time Shift/Assets/Scripts/Enviornment/OpenDoor.cs
@@ -5,25 +5,42 @@ using UnityEngine;
 public class OpenDoor : ObjectsInteraction
 {
     [SerializeField] private Animator animator;
+    [SerializeField] private float doorTransitionTime = 1f;
     private DoorState _doorState = DoorState.Closed;
     
     public enum DoorState
     {
         Open,
         Closed,
+        Moving,
     }
     
     public override void DoInteraction()
     {
+        if (_doorState == DoorState.Moving)
+            return;
+        
         if (_doorState == DoorState.Closed)
         {
             animator.Play("DoorOpen");
-            _doorState = DoorState.Open;
+            _doorState = DoorState.Moving;
+            Invoke(nameof(UpdateOpenState), doorTransitionTime);
         }
         else
         {
             animator.Play("DoorClose");
-            _doorState = DoorState.Closed;
+            _doorState = DoorState.Moving;
+            Invoke(nameof(UpdateClosedState), doorTransitionTime);
         }
+    }
+
+    public void UpdateOpenState()
+    {
+        _doorState = DoorState.Open;
+    }
+
+    private void UpdateClosedState()
+    {
+        _doorState = DoorState.Closed;
     }
 }
